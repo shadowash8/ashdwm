899ae09ac8414e0645ecff5ad6dff32bf59682c0
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2026-01-23 20:59:07 +0530

    slock: xresources pathc

--- slock/config.h
+++ slock/config.h
@@ -1,12 +1,21 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INIT] = "black",       /* after initialization */
+	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+};
+
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
