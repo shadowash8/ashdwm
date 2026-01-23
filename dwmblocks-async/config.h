5279c36cb69f761adcd6526366f30e33b01419c2
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2026-01-23 09:04:27 +0530

    update dwm blocks rm icons back to tui look

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER " | "
+#define DELIMITER "  "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 64
@@ -11,17 +11,17 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 0
+#define LEADING_DELIMITER 1
 
 // Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 0
+#define TRAILING_DELIMITER 1
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("  ", "org-clock-task", 30, 12) \
+    X("[T] ", "org-clock-task", 30, 12) \
     X("", "sb-network", 20, 5) \
-    X("", "sb-volume", 1, 4) \
-    X("", "sb-battery", 30, 3) \
-    X("󰃭 ", "date '+%a %b %d %I:%M %p'", 30, 1) \
+    X("[V] ", "sb-volume", 1, 4) \
+    X("[B] ", "sb-battery", 30, 3) \
+    X("[D] ", "date '+%a %b %d %I:%M %p'", 30, 1) \
 
 #endif  // CONFIG_H
